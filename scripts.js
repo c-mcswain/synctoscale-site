42f17351839bd6c85a0ad5e1496eb879f625d7bb
c-mcswain <christymcswain@icloud.com>
2025-12-07 19:43:15 -0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".faq-card");
+
+  cards.forEach(card => {
+    const question = card.querySelector(".faq-question");
+    question.addEventListener("click", () => {
+      card.classList.toggle("active");
+    });
+  });
+});
